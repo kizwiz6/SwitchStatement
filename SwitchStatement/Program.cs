965d80e08624645b445ec77d01fc92a19143d787
Kieran Emery <kizwiz@hotmail.co.uk>
2022-01-13 01:40:41 +0000

    Add project files.

---
+++ SwitchStatement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* SYNTAX:
+             * switch (expression)
+                {
+
+                case constant-expression;
+                break;
+
+
+                default :
+                statement
+
+                break;
+
+                }
+            */
+
+
+        }
+    }
+}
